a7e84a0372ddb6b10e5741634f7d6cac78e779b3
RangareddyJulakanti <rangareddyjava9@gmail.com>
2022-05-08 17:11:09 +0530

    Exception handling

--- src/main/java/com/exceptionHandling/Test.java
+++ src/main/java/com/exceptionHandling/Test.java
@@ -10,62 +10,67 @@ import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class Test {
     public static void main(String[] args) {
-      List<Student> studentList=getDataFromUI();
 
-      Fault fault=validateAllStudents(studentList);
+        List<Student> studentList = getDataFromUI();
+        List<Student> validStudentData = new ArrayList<>();
+        List<String> errorMessageList = new ArrayList<>();
 
-      if(CollectionUtils.isEmpty(fault.getErrorMessages())) {
-          persistStudentDataIntoDB(studentList);
-      }else{
-          List<String> errorMessageList=new ArrayList<>();
-          for(ErrorMessage errorMessage:fault.errorMessages){
-              errorMessageList.add(errorMessage.getErrorMessage());
-          }
-         String totalMessages= errorMessageList.stream().collect(Collectors.joining("\n"));
-         throw  new RuntimeException(totalMessages);
-      }
+        for (Student student : studentList) {
+            ErrorMessage errorMessage = validateAllStudents(student);
+            if (Objects.nonNull(errorMessage)) {
+                errorMessageList.add(errorMessage.getErrorMessage());
+            } else {
+                validStudentData.add(student);
+            }
+        }
+        printInvalidData(errorMessageList);
+        persistStudentDataIntoDB(validStudentData);
+    }
+
+    private static void printInvalidData(List<String> errorMessageList) {
+        errorMessageList.forEach(System.out::println);
     }
 
     private static void persistStudentDataIntoDB(List<Student> studentList) {
         System.out.println("Valid Student details");
-        for (Student student: studentList) {
-            System.out.println("StudentId"+student.getId()+" StudentName =  "+student.getName()+"CollegeName= "+student.getCollegeName());
+        for (Student student : studentList) {
+            System.out.println("StudentId" + student.getId() + " StudentName =  " + student.getName() + "CollegeName= " + student.getCollegeName());
         }
     }
 
-    private static  Fault  validateAllStudents(List<Student> studentList) {
-        Fault fault=new Fault();
-        for(Student student:studentList){
-            if(StringUtils.isEmpty(student.getCollegeName())){
-                ErrorMessage errorMessage=new ErrorMessage();
-                errorMessage.setErrorMessage("Invalid collegeName = " +student.getCollegeName() + " for the Student id= "+student.getId());
-                errorMessage.setErrorCode("BAD Request");
-                fault.addErrorMessage(errorMessage);
-            }
-            if(StringUtils.isEmpty(student.getName())){
-                ErrorMessage errorMessage=new ErrorMessage();
-                errorMessage.setErrorMessage("Invalid studentName = " +student.getName() + " for the Student id= "+student.getId());
-                errorMessage.setErrorCode("BAD Request");
-                fault.addErrorMessage(errorMessage);
-            }
+    private static ErrorMessage validateAllStudents(Student student) {
+
+        if (StringUtils.isEmpty(student.getCollegeName())) {
+            ErrorMessage errorMessage = new ErrorMessage();
+            errorMessage.setErrorMessage("Invalid collegeName = " + student.getCollegeName() + " for the Student id= " + student.getId());
+            errorMessage.setErrorCode("BAD Request");
+            return errorMessage;
         }
-        return fault;
+        if (StringUtils.isEmpty(student.getName())) {
+            ErrorMessage errorMessage = new ErrorMessage();
+            errorMessage.setErrorMessage("Invalid studentName = " + student.getName() + " for the Student id= " + student.getId());
+            errorMessage.setErrorCode("BAD Request");
+            return errorMessage;
+        }
+        return null;
     }
 
     private static List<Student> getDataFromUI() {
         return Arrays.asList(
-                new Student(1,"ranga","A"),
-                new Student(2,"raju",null),
-                new Student(3,"mohan","C"),
-                new Student(4,"","D")
+                new Student(1, "ranga", "A"),
+                new Student(2, "raju", null),
+                new Student(3, "mohan", "C"),
+                new Student(4, "", "D")
         );
     }
 }
-class Student{
+
+class Student {
     private final int id;
     private final String name;
     private final String collegeName;
